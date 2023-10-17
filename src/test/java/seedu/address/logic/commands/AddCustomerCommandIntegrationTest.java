b4a32e8646d808ec75e20a24459b5b3b176365c5
FerdiHS <ferdi.fhs.fhs@gmail.com>
2023-10-18 00:25:11 +0800

    Fix unit testing for add customer command

--- src/test/java/seedu/address/logic/commands/AddCustomerCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCustomerCommandIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.PropertyBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.customer.Customer;
 import seedu.address.testutil.CustomerBuilder;
@@ -23,14 +24,14 @@ public class AddCustomerCommandIntegrationTest {
 
     @BeforeEach
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model = new ModelManager(getTypicalAddressBook(), new PropertyBook(), new UserPrefs());
     }
 
     @Test
     public void execute_newCustomer_success() {
         Customer validCustomer = new CustomerBuilder().build();
 
-        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(model.getAddressBook(), new PropertyBook(), new UserPrefs());
         expectedModel.addCustomer(validCustomer);
 
         assertCommandSuccess(new AddCustomerCommand(validCustomer), model,
