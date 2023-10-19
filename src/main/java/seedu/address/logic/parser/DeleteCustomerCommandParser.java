b7221523b2cdf8503bd9886cb28b0546259d7326
saraozn <saraongzining@gmail.com>
2023-10-19 15:02:11 +0800

    fix comments

--- src/main/java/seedu/address/logic/parser/DeleteCustomerCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCustomerCommandParser.java
@@ -9,13 +9,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteCustomerCommand object
  */
 public class DeleteCustomerCommandParser implements Parser<DeleteCustomerCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteCustomerCommand
+     * and returns a DeleteCustomerCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCustomerCommand parse(String args) throws ParseException {
