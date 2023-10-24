5209a54c7eec6add75c0d65377a2c2274c7e601a
jianrong7 <ipwnjianrong@gmail.com>
2023-10-24 20:33:14 +0800

    Add tests for listprop feature

---
+++ src/test/java/seedu/address/logic/commands/ListPropertyCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showCustomerAtIndex;
+import static seedu.address.testutil.TypicalCustomers.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CUSTOMER;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.PropertyBook;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListPropertyCommand.
+ */
+public class ListPropertyCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new PropertyBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new PropertyBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListPropertyCommand(), model, ListPropertyCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showCustomerAtIndex(model, INDEX_FIRST_CUSTOMER);
+        assertCommandSuccess(new ListPropertyCommand(), model, ListPropertyCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
