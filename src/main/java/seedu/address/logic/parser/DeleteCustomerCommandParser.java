2b89c4323b234de4688ff9a3920ead1c15c58a71
saraozn <saraongzining@gmail.com>
2023-10-24 23:36:23 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/parser/DeleteCustomerCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCustomerCommandParser.java
@@ -6,8 +6,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCustomerCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-
-
 /**
  * Parses input arguments and creates a new DeleteCustomerCommand object
  */
